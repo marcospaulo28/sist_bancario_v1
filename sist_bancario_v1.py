9f82151dbc75673ca745711fbec41e7e76307177
marcospaulo28 <marcospaulo28@gmail.com>
2023-08-05 18:19:15 -0300

    commit final

--- sist_bancario_v1.py
+++ sist_bancario_v1.py
@@ -10,7 +10,8 @@ menu = """
 saldo = 0
 limite = 500
 extrato = ""
-numero_saques = 3
+numero_saques = 0
+LIMITE_SAQUES = 3
 
 while True:
     
@@ -43,3 +44,23 @@ while True:
 
         elif excedeu_saques:
             print("Operação falou! Número máximo de saques excedido.") 
+        
+        elif valor > 0:
+            saldo -= valor
+            extrato +=f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+
+        else:
+            ("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "e":
+        print("\n==================EXTRATO===================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("=============================================")
+
+    elif opcao == "q":
+        break
+    
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada")
